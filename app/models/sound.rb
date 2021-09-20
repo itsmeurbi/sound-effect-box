6bfda26d244e11442dae82b8a93f9595550030a0
Eli Sanchez <elisanchez@MacBook-Air-de-Eli.local>
2021-09-19 22:10:51 -0500

    Add sound type form input

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -2,5 +2,7 @@ class Sound < ApplicationRecord
     has_one_attached :image
     has_one_attached :sound_file
 
-    enum type: %w[happy sad epic fun]
+    enum sound_type: %w[happy sad epic fun]
+
+    validates_presence_of :image, :sound_type, :sound_file
 end
